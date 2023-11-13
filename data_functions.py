c68eca4a61e2630890513421af3a14fcdd07583e
Scott O <sskohara@icloud.com>
2023-11-12 19:07:45 -0500

    Added a way to sort by monthly weather data. Created two new functions for this purpose and added a new selection box on the Daily Weather section of the website. The monthly weather code still needs optimizing and cleaning.

--- data_functions.py
+++ data_functions.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import calendar
 weather = pd.read_csv("climate-daily.csv", low_memory = False)
 
-def day_mean(day, month, column):
+def day_mean(day, month, column, rounded = True):
     ''' finds the mean value of a column on a specific day
         ex. average high temperature on March 7
 
@@ -15,6 +15,8 @@ def day_mean(day, month, column):
             day: the numerical day to use, int
             month: the numerical month to use, int
             column: the column on the .csv to sort by (ex. 'MEAN_TEMPERATURE'), string
+            rounded: an optional argument to round the averages to two decimal points
+                set to true by default, boolean
         
         return:
             the numerical mean on that column and day
@@ -23,7 +25,49 @@ def day_mean(day, month, column):
     df_oneday = weather.loc[(weather["LOCAL_MONTH"] == month) & (weather["LOCAL_DAY"] == day)]
     mean_value = df_oneday[column].mean()
 
-    return mean_value
+    if rounded:
+        return round(mean_value, 2)
+
+def month_mean(month, column, rounded = True):
+    ''' find the value of a column for a daily average of a full month
+    ex. average amount of daily snow in December
+
+    arguments:
+        month: the numerical month, int
+        column: the column on the .csv to sort by (ex. 'SNOW_ON_GROUND), string
+        rounded: an optional argument to round the averages to two decimal points
+                set to true by default, boolean
+
+    return:
+        the daily average of the column value for that month
+    '''
+
+    df_onemonth = weather.loc[(weather["LOCAL_MONTH"] == month)]
+    mean_value = df_onemonth[column].mean()
+
+    if round:
+        return round(mean_value, 2)
+    else:
+        return mean_value
+
+def month_mean_dict(column, month_num = True):
+    ''' creates a dictionary for the means of each month, sorting for a column
+
+    argument:
+        column: the column on the .csv to sort by (ex. 'MEAN_TEMPERATURE')
+        month_num: an optional argument to have the dictionary keys be month numbers
+                set to true by default, boolean
+
+    return:
+        a dictionary with each month matched up to the mean value for the column on that month
+    '''
+
+    if month_num == True:
+        month_means = {month : month_mean(month, column) for month in range(1,13)}
+    else:
+        month_means = {calendar.month_name[month] : month_mean(month, column) for month in range(1,13)}
+
+    return month_means
 
 @st.cache_data
 def convert_df(dataframe):
--- streamlit_webpage.py
+++ streamlit_webpage.py
@@ -8,7 +8,7 @@ import math
 import calendar
 import io
 
-from data_functions import day_mean, convert_df
+from data_functions import day_mean, convert_df, month_mean, month_mean_dict
 sns.set(color_codes = True)
 
 # using pandas to open the csv file
@@ -26,7 +26,7 @@ st.set_page_config(page_title = "Historical Weather Network", layout = "wide")
 with st.sidebar:
     selected = option_menu(
         menu_title = "Navigation",
-        options = ["Home", "Today's Weather", "Interesting Graphs", "Extra", "Future Forecast"]
+        options = ["Home", "Daily Weather", "Interesting Graphs", "Extra", "Future Forecast"]
     )
 
 # set up the available days and months that can be selected
@@ -56,35 +56,87 @@ if selected == "Home":
         # printing the sum of null values for each column in the weather dataframe
         st.text(weather.isnull().sum())
 
-elif selected == "Today's Weather":
+elif selected == "Daily Weather":
     st.header("Today's Historical Weather Report")
     current_date = datetime.today()
     # display the current date and some basic info for that date
     st.subheader(f"Today is {calendar.month_name[current_date.month]} {current_date.day}, {current_date.year}")
     st.write(f"The average temperature for today is {round(day_mean(current_date.day, current_date.month, 'MEAN_TEMPERATURE'),1)} °C")
 
+    st.write()
+
     # specific date information gathering
-    st.subheader("Enter the Date")
-    with st.form("entry_form"):
+    st.subheader("Enter a specific date and info to sort by")
+    with st.form("day_entry"):
         # setting up the three column selection options for specific date information gathering
-        info_type, day_select, month_select = st.columns(3)
-        month_select.selectbox("Select Month:", months, key = "month")
-        day_select.selectbox("Select Day:", days, key = "day")
-        info_type.selectbox("Select Info:", ["MEAN_TEMPERATURE", "MAX_TEMPERATURE",
-        "MIN_TEMPERATURE", "TOTAL_PRECIPITATION"], key = "header")
+        info_type_day, day_select, month_select_day = st.columns(3)
+        month_select_day.selectbox("Select Month:", months, index = current_date.month - 1, key = "days_month")
+        day_select.selectbox("Select Day:", days, index = current_date.day - 1, key = "day")
+        info_type_day.selectbox("Select Info:", ["MEAN_TEMPERATURE", "MAX_TEMPERATURE",
+        "MIN_TEMPERATURE", "TOTAL_PRECIPITATION", "SNOW_ON_GROUND"], key = "days_header")
 
         # button to submit choices and see results
-        submitted = st.form_submit_button("Find Info:")
-        if submitted: 
+        day_submit = st.form_submit_button("Find Info:")
+        if day_submit: 
             # avg outputting
             used_day = st.session_state["day"]
-            used_month = months.index(st.session_state["month"]) + 1
-            used_header = str(st.session_state["header"])
+            used_month = months.index(st.session_state["days_month"]) + 1
+            used_days_header = str(st.session_state["days_header"])
             # accounting for invalid dates (ex. feb 30)
-            if math.isnan(day_mean(used_day, used_month, used_header)):
+            if math.isnan(day_mean(used_day, used_month, used_days_header)):
                 st.write("This is an invalid date")
             else:
-                st.write(f"{day_mean(used_day, used_month, used_header)}")
+                st.write(f"{day_mean(used_day, used_month, used_days_header)}")
+    
+    # month information gathering
+    st.subheader("Enter the specifications for the month sort")
+    with st.form("month_entry"):
+        # setting up the three column selection options for month information gathering
+        info_type_month, month_select_month, output_type = st.columns(3)
+        # creating a copy of months so I can include an "All Months" options
+        aug_months = months.copy()
+        aug_months.insert(0,"All Months")
+        # select boxes for month entry
+        month_select_month.selectbox("Select Month:", aug_months, index = current_date.month,
+        key = "months_month")
+        info_type_month.selectbox("Select Info:", ["MEAN_TEMPERATURE", "MAX_TEMPERATURE",
+        "MIN_TEMPERATURE", "TOTAL_PRECIPITATION", "SNOW_ON_GROUND"], key = "months_info")
+        output_type.selectbox("Output Type (Only for 'All Months'):", ["None", "Dict", "Max", "Min"], key = "output_type")
+
+        # submit box for month entry
+        month_submit = st.form_submit_button("Find Info:")
+        if month_submit:
+            if not st.session_state["output_type"] == "None" and st.session_state["months_month"] == "All Months":
+                if st.session_state["output_type"] == "Dict":
+                    for key, value in month_mean_dict(st.session_state["months_info"], month_num = False).items():
+                        st.write(f"{key} : {value}")
+
+                elif st.session_state["output_type"] == "Max":
+                    max_value = -999
+                    max_key = ""
+                    for key, value in month_mean_dict(st.session_state["months_info"], month_num = False).items():
+                        if value > max_value:
+                            max_value = value
+                            max_key = key
+                    st.write(f"{max_key} : {max_value}")
+
+                elif st.session_state["output_type"] == "Min":
+                    max_value = 999
+                    max_key = ""
+                    for key, value in month_mean_dict(st.session_state["months_info"], month_num = False).items():
+                        if value < max_value:
+                            max_value = value
+                            max_key = key
+                    st.write(f"{max_key} : {max_value}")
+            elif st.session_state["output_type"] == "None" and not st.session_state["months_month"] == "All Months":
+                used_month_months = months.index(st.session_state["months_month"]) + 1
+                used_months_header = st.session_state["months_info"]
+                st.write(f"{month_mean(used_month_months, used_months_header)}")
+            else:
+                st.write("Invalid input. Please try again.")
+
+
+
 
 
 elif selected == "Interesting Graphs":
